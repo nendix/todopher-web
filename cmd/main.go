39318caf7f89243470309c3fdb7137db9a20f06b
nendix <nendix12@gmail.com>
2025-02-19 22:09:50 +0100

    serve on IP /

--- cmd/main.go
+++ cmd/main.go
@@ -19,28 +19,25 @@ func main() {
 	// Initialize Gin router
 	r := gin.Default()
 	r.LoadHTMLGlob("web/templates/*.html")
-	r.Static("/todopher/static", "web/static")
+	r.Static("/static", "web/static")
 
 	// Gruppo di rotte con prefisso /todopher
-	todo := r.Group("/todopher")
-	{
 
-		// Route for the main index page
-		todo.GET("/", handlers.GetIndex)
+	// Route for the main index page
+	r.GET("/", handlers.GetIndex)
 
-		// API routes for todos
-		todo.GET("/todos", handlers.GetTodos)
-		todo.GET("/todos/pending", handlers.GetPendingTodos)
-		todo.GET("/todos/completed", handlers.GetCompletedTodos)
-		todo.GET("/todos/:id", handlers.GetTodoByID)
-		todo.GET("/todos/search", handlers.SearchTodos)
-		todo.GET("/search-form", handlers.GetSearchForm)
-		todo.GET("/todos/edit/:id", handlers.GetEditTodo)
-		todo.POST("/todos", handlers.CreateTodo)
-		todo.PATCH("/todos/:id", handlers.UpdateTodo)
-		todo.PATCH("/todos/:id/toggle", handlers.UpdateTodoStatus)
-		todo.DELETE("/todos/:id", handlers.DeleteTodo)
-	}
+	// API routes for todos
+	r.GET("/todos", handlers.GetTodos)
+	r.GET("/todos/pending", handlers.GetPendingTodos)
+	r.GET("/todos/completed", handlers.GetCompletedTodos)
+	r.GET("/todos/:id", handlers.GetTodoByID)
+	r.GET("/todos/search", handlers.SearchTodos)
+	r.GET("/search-form", handlers.GetSearchForm)
+	r.GET("/todos/edit/:id", handlers.GetEditTodo)
+	r.POST("/todos", handlers.CreateTodo)
+	r.PATCH("/todos/:id", handlers.UpdateTodo)
+	r.PATCH("/todos/:id/toggle", handlers.UpdateTodoStatus)
+	r.DELETE("/todos/:id", handlers.DeleteTodo)
 
 	// Start server
 	r.SetTrustedProxies(nil)
